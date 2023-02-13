e8cb4b00fc75df32f089d485780b341fe2fbeb58
mustophy <54689763+mustophy@users.noreply.github.com>
2023-02-13 12:04:51 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,20 +1,20 @@
 const Footer = () => {
     return (
-        <div className="h-[184px] bg-footer pt-[26px] pb-[23px] px-3 sm:px-[23px] sm:flex">
+        <div className="h-[184px] pt-[26px] pb-[23px] px-3 sm:px-[23px] sm:flex">
             <div className="sm:w-1/3">
-                <p className="text-white">PRESS ATTACK</p>
+                <p className="">PRESS ATTACK</p>
                 <p className="text-[#1F215F] italic">TRACKER</p>
             </div>
             <ul className="mt-4 sm:mt-0 sm:w-1/3 flex flex-row sm:flex-col justify-between">
-                <li className="text-[18px] text-white leading-[22px]">Home</li>
-                <li className="text-[18px] text-white leading-[22px]">Stories</li>
-                <li className="text-[18px] text-white leading-[22px]">Report Attack</li>
-                <li className="text-[18px] text-white leading-[22px]">Blog</li>
+                <li className="text-[18px]  leading-[22px]">Home</li>
+                <li className="text-[18px]  leading-[22px]">Stories</li>
+                <li className="text-[18px]  leading-[22px]">Report Attack</li>
+                <li className="text-[18px]  leading-[22px]">Blog</li>
             </ul>
             <ul className="mt-4 sm:mt-0 sm:w-1/3 flex flex-row sm:flex-col justify-between">
-                <li className="text-[18px] text-white leading-[22px] mb-4">Get in touch</li>
-                <li className="text-[18px] text-white leading-[22px] mb-4">info@ptcij.org</li>
-                <li className="text-[18px] text-white leading-[22px]">08104198112</li>
+                <li className="text-[18px]  leading-[22px] mb-4">Get in touch</li>
+                <li className="text-[18px]  leading-[22px] mb-4">info@ptcij.org</li>
+                <li className="text-[18px]  leading-[22px]">08104198112</li>
             </ul>
         </div>
     )
